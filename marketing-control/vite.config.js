13c657b37625ce5cc0183e1fe42360e676dd4ff0
Carter Barns <barnscarter@gmail.com>
2026-08-30 15:53:47 -0500

    fix(marketing-control): allow Tailscale Serve MagicDNS host in Vite

--- marketing-control/vite.config.js
+++ marketing-control/vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Tailscale Serve proxies as https://cmb-workbench.tailf72e3f.ts.net:5188
+    allowedHosts: ['.tailf72e3f.ts.net'],
+  },
 });
